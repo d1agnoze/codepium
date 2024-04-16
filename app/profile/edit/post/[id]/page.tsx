b49d308b4352ce970730a4cf84dfbaa7cae10f7b
vdac <dacvuong202@gmail.com>
2024-04-16 09:05:40 +0700

    Improve code readability

--- app/profile/edit/post/[id]/page.tsx
+++ app/profile/edit/post/[id]/page.tsx
@@ -4,16 +4,16 @@ import ThreadEditForm, {
   Prop,
 } from "@/components/edit/Question_Post_Edit_Component";
 import { Post } from "@/types/post.type";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+import Supabase from "@/utils/supabase/server-action";
+import { getUser } from "@/utils/supabase/user";
 import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { id: string } }) {
   try {
     /* INFO: fetch data from supabase, throw any Error encounterd */
-    const supabase = createServerComponentClient({ cookies: () => cookies() });
+    const sb = Supabase();
     /** @description fetch data */
-    const { data, error } = await supabase
+    const { data, error } = await sb
       .from("get_post_full")
       .select()
       .eq("id", params.id)
@@ -21,23 +21,25 @@ export default async function Page({ params }: { params: { id: string } }) {
       .limit(1)
       .maybeSingle<Post>();
 
+    if (error || !data) throw new Error("Error fetching data");
+    if (data.isDeleted) notFound();
+
     /** @description fetch user data */
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
+    const user = await getUser(sb);
 
     /**
      * @description Get user authorization
      */
-    const fromUser = data?.user_id && user?.id && user?.id === data?.user_id;
-    if (error || data == null || !fromUser || data.isDeleted) notFound();
+    const fromUser = user.id === data.user_id;
+    if (!fromUser) notFound();
 
     /**
      * @description Get Expertise tags
      */
-    const { data: tags, error: tag_err } = await supabase
+    const { data: tags, error: tag_err } = await sb
       .rpc("get_expertises_set", { uuids: data!.tag })
       .returns<Expertise[]>();
+
     if (tag_err || !tags) throw new Error("Error fetching expertise tags");
 
     const prop: Prop = {
@@ -47,8 +49,8 @@ export default async function Page({ params }: { params: { id: string } }) {
         title: data?.title ?? "",
         content: data?.content ?? "",
         expertises:
-          tags!.map((item) => {
-            return { id: item.id, display_name: item.display_name };
+          tags?.map((x) => {
+            return { id: x.id, display_name: x.display_name };
           }) ?? [],
       },
     };
